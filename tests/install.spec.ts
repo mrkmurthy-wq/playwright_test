c103d4f515d682501098ee5b6d53a556b13a07a3
krishna <krishna@krishnas-MacBook-Pro-2.local>
2026-08-05 00:43:04 -0500

    delete

--- tests/install.spec.ts
+++
@@ -1,7 +0,0 @@
-import {test, expect} from '@playwright/test'
-test('install test', async ({page}) => {
-    await page.goto('https://www.google.com/');
-    await page.getByRole('combobox', { name: 'Search' }).fill('test');
-    await page.getByRole('button', { name: 'Google Search' }).press('Enter');
-    await expect(page.locator('iframe[name="*"]').contentFrame().getByRole('checkbox', { name: 'I\'m not a robot' })).toBeVisible()
-})
